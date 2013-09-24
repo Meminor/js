9e3cce984da7fbfc3e3f9aa6155f413def18ef14
Kim Bul <tmeminor@gmail.com>
2013-09-25 07:38:11 +0900

    button is styled

--- pro_js/js/main.js
+++ pro_js/js/main.js
@@ -1,4 +1,20 @@
-$(document).ready(function() {
-    $('#buttonDiv input:submit').hide();
-    $('<a href=#>Submit Order</a>').appendTo("#buttonDiv");
-})
+(function($){
+    $(function(){
+        var $btnD = $("#buttonDiv");
+        
+        $btnD.find("input:submit").hide();
+
+        $("<a href=#>Order</a>").
+            appendTo($btnD).
+            addClass('button').
+            click(function(){
+                $('form').submit();
+            }).
+            hover(
+                function(){
+                    $btnD.find("a").addClass('button-hover');
+                }, function(){
+                    $btnD.find("a").removeClass('button-hover');
+            });
+    });
+})(jQuery);
