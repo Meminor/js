7a8f3677f25bc770845db31e13f4f180d5098756
Meminor <tmeminor@gmail.com>
2013-09-25 10:58:33 +0900

    clean up

--- node/read_write/write_sync.js
+++
@@ -1,6 +0,0 @@
-var fs = require("fs");
-console.log("Starting");
-fs.writeFile("write_sync.txt", "Hello World! Syncronous!", function(error){
-    console.log("Written file");
-});
-console.log("Finished");
