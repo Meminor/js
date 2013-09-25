7975fb487d6f5bb7f854ebbb5fb7b48cd4a76dba
Kim Bul <tmeminor@gmail.com>
2013-09-26 07:57:02 +0900

    laptop category is added

--- pro_js/view_model/js/view.js
+++ pro_js/view_model/js/view.js
@@ -1,24 +1,49 @@
 (function($){
     var pcModel = {
-            category: "Desktop",
-            items: [
-                {
-                    id: "apple",
-                    name: "Apple",
-                    price: 1299
-                },
-                {
-                    id: "lenovo",
-                    name: "Lenovo",
-                    price: 980
-                },
-                {
-                    id: "dell",
-                    name: "Dell",
-                    price: 1100
-                }
-            ]
-        };
+        products: [
+            {
+                category: "Desktop",
+                items: [
+                    {
+                        id: "apple",
+                        name: "Apple",
+                        price: 1299
+                    },
+                    {
+                        id: "lenovo",
+                        name: "Lenovo",
+                        price: 980
+                    },
+                    {
+                        id: "dell",
+                        name: "Dell",
+                        price: 1100
+                    }
+                ]
+            },
+            {
+                category: "Laptop",
+                items: [
+                    {
+                        id: "sony",
+                        name: "Sony",
+                        price: 1299
+                    },
+                    {
+                        id: "msi",
+                        name: "MSI",
+                        price: 980
+                    },
+                    {
+                        id: "toshiba",
+                        name: "Toshiba",
+                        price: 1100
+                    }
+                ]
+            },
+
+        ]
+    };
 
         ko.applyBindings(pcModel);
 })(jQuery);
