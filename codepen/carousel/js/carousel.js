a124feb3ab3ea51dd06795af37132f8a9e449d47
Meminor <tmeminor@gmail.com>
2013-09-28 21:20:03 +0900

    carousel addition

--- codepen/carousel/js/carousel.js
+++ codepen/carousel/js/carousel.js
@@ -1,17 +1,52 @@
 (function(){
     var imgs = document.querySelectorAll('.content ul li img'),
         btn = document.getElementsByAttribute('data-role'),
-        currentImg = 0;
+        currentImg = 0,
+        btnObj = {};
+
 
     function initCar() {
         resetCar();
-
+        // button();
         imgs[0].style.display = 'block';
     }
 
     function resetCar() {
         var i;
 
+        for (i = 0; i < imgs.length; i++) {
+            imgs[i].style.display = 'none';
+        }
+    }
+
+    for (var i = 0; i < btn.length; i++) {
+        btn[i].addEventListener('click', function(){
+            if (this.dataset.role === "left") {
+                if (currentImg === 0) {
+                    currentImg = imgs.length;
+                }
+            toLeft();
+            } else {
+                if )
+            }
+
+        }, false);
+    }
+
+    function toLeft() {
+        resetImgs();
+        imgs[currentImg - 1].style.display = 'block';
+        currentImg--;
+    }
+
+    function toRight() {
+        resetImgs();
+        imgs[currentImg + 1].style.display = "block";
+        currentImg++;
+    }
+
+    function resetImgs() {
+        var i;
         for (i = 0; i < imgs.length; i++) {
             imgs[i].style.display = 'none';
         }
