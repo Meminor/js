18a959b593678ede2611839bcdf05510351d9bd1
Meminor <tmeminor@gmail.com>
2013-09-30 17:59:33 +0900

    panoramia js cleanup

--- ajax/panoramio/js/main-.js
+++ ajax/panoramio/js/main-.js
@@ -1,6 +1,6 @@
 (function($){
     $(function(){
-        var tmpImg = $('#tpl-img'),
+        var tmpImg = $('#tpl-img').html(),
             searchBox = $('#search-box');
 
         function setTmp(tmp, sKey, sValue) {
@@ -13,17 +13,17 @@
 
             $.getJSON(url, function(data) {
                 var sHTML = '',
-                    sTmp = tmpImg.html();
+                    i;
 
                 if (data.photos.length === 0) {
                     alert('No images tagged "' + search + '"');
                 } else {
-                    for (var i=0; i < data.photos.length; i++) {
+                    for (i=0; i < data.photos.length; i++) {
                         var oPhoto = data.photos[i].photoPixelsUrls,
                             src = $.trim(oPhoto[0].url);
 
                         if (src !== '') {
-                            sHTML += setTmp(sTmp, 'src', src);
+                            sHTML += setTmp(tmpImg, 'src', src);
                             sHTML = setTmp(sHTML, 'ownerUrl', data.photos[1].ownerUrl);
                             sHTML = setTmp(sHTML, 'ownerName', data.photos[i].ownerName);
                         };
