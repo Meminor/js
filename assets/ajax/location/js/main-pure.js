11c68eaa4d96f84e2e002a90d61a34f7c656b739
Kim Bul <tmeminor@gmail.com>
2013-10-07 22:03:11 +0900

    mini cleanup

--- assets/ajax/location/js/main-pure.js
+++
@@ -1,8 +0,0 @@
-var button = document.getElementsByTagName('input')[0],
-    message = document.getElementsByClassName('my-place')[0];
-
-console.log(button);
-
-button.onclick = function(e) {
-    message.innerHTML = "loading...";
-}
